16e54e10cc62945f8afdfeffe60488482cb2c07e
YamaD <me@dyama.net>
2022-05-20 18:11:13 +0900

    added router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,16 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
 import Index from "./routes/Index"
 
 const App = () => (
-  <>
-    <Index />
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+    </Routes>
+  </BrowserRouter>
 )
 
 export default App
